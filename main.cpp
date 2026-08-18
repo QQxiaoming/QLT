4c9a6e7a9cfd69bf7d28d849ba92290160a9a091
qiaoqiming <qiaoqiming@percipio.xyz>
2026-08-18 14:32:47 +0800

    Refactor startup sequence to use structured steps and improve expression handling
    Signed-off-by: qiaoqiming <qiaoqiming@percipio.xyz>

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <QMetaObject>
 #include <QThread>
 #include <QTimer>
+#include <array>
 #include <cstdio>
+#include <functional>
 
 #include "GestureThread.h"
 #include "HardwareHal.h"
@@ -26,57 +28,86 @@ int main(int argc, char *argv[])
     LedThread ledThread(&hal);
     ledThread.start();
 
+    const auto setFaceExpression = [&face](RobotFaceWidget::Expression expression) {
+        QMetaObject::invokeMethod(&face, [expression, &face] {
+            face.setExpression(expression);
+        }, Qt::QueuedConnection);
+    };
+    struct StartupStep {
+        int durationMs;
+        std::function<void()> action;
+    };
+    const std::array<StartupStep, 4> startupSequence = {{
+        {2500, [&ledThread, setFaceExpression] {
+            ledThread.cmd("rainbow_flow");
+            setFaceExpression(RobotFaceWidget::Expression::Happy);
+        }},
+        {1800, [&ledThread, setFaceExpression] {
+            ledThread.cmd("fixed 255 100 180");
+            setFaceExpression(RobotFaceWidget::Expression::Cute);
+        }},
+        {3000, [&ledThread, setFaceExpression] {
+            ledThread.cmd("blink 255 80 140 350");
+            setFaceExpression(RobotFaceWidget::Expression::Shy);
+        }},
+        {0, [&ledThread, setFaceExpression] {
+            ledThread.cmd("rainbow_flow");
+            setFaceExpression(RobotFaceWidget::Expression::Happy);
+        }},
+    }};
+
     QThread startupThread;
     QObject startupWorker;
+    QTimer startupTimer(&startupWorker);
+    startupTimer.setSingleShot(true);
+    std::size_t startupStep = 0;
     startupWorker.moveToThread(&startupThread);
-    QObject::connect(&startupThread, &QThread::started, &startupWorker,
-                     [&face, &ledThread, &startupThread, &startupWorker] {
-                         const auto setFaceExpression = [&face](RobotFaceWidget::Expression expression) {
-                             QMetaObject::invokeMethod(&face, [expression, &face] {
-                                 face.setExpression(expression);
-                             }, Qt::QueuedConnection);
-                         };
+    const auto applyStartupStep = [&startupSequence, &startupTimer](std::size_t index) {
+        const StartupStep& step = startupSequence[index];
+        step.action();
+        if (step.durationMs > 0) {
+            startupTimer.start(step.durationMs);
+        }
+    };
+    const auto restartStartupSequence = [&startupStep, &startupTimer, applyStartupStep] {
+        startupStep = 0;
+        startupTimer.stop();
+        applyStartupStep(startupStep);
+    };
+    QObject::connect(&startupTimer, &QTimer::timeout, &startupWorker,
+                     [&startupSequence, &startupStep, &startupTimer, applyStartupStep] {
+                         ++startupStep;
+                         if (startupStep >= startupSequence.size()) {
+                             startupTimer.stop();
+                             return;
+                         }
 
-                         ledThread.cmd("rainbow_flow");
-                         setFaceExpression(RobotFaceWidget::Expression::Happy);
-
-                         QTimer::singleShot(2500, &startupWorker, [&ledThread, setFaceExpression] {
-                             ledThread.cmd("fixed 255 100 180");
-                             setFaceExpression(RobotFaceWidget::Expression::Cute);
-                         });
-                         QTimer::singleShot(5000, &startupWorker, [&ledThread, setFaceExpression] {
-                             ledThread.cmd("blink 255 80 140 350");
-                             setFaceExpression(RobotFaceWidget::Expression::Shy);
-                         });
-                         QTimer::singleShot(7500, &startupWorker, [&ledThread, setFaceExpression] {
-                             ledThread.cmd("rainbow_flow");
-                             setFaceExpression(RobotFaceWidget::Expression::Happy);
-                         });
-                         QTimer::singleShot(9000, &startupWorker, [&startupThread] {
-                             startupThread.quit();
-                         });
+                         applyStartupStep(startupStep);
                      });
+    QObject::connect(&startupThread, &QThread::started, &startupWorker, restartStartupSequence);
     QObject::connect(&app, &QCoreApplication::aboutToQuit, &startupThread, &QThread::quit);
     startupThread.start();
 
     qRegisterMetaType<HardwareHal::HeadPetGesture>("HardwareHal::HeadPetGesture");
     GestureThread gestureThread(&hal);
     QObject::connect(&gestureThread, &GestureThread::gestureDetected, &app,
-                     [&face](HardwareHal::HeadPetGesture gesture) {
+                     [&face, &startupWorker, restartStartupSequence](HardwareHal::HeadPetGesture gesture) {
                          switch (gesture) {
                          case HardwareHal::HeadPetGesture::Press:
-                             std::printf("gesture: press\n");
-                             face.setExpression(RobotFaceWidget::Expression::Custom);
-                             face.setCustomImage("/root/1.jpg");
+                             //std::printf("gesture: press\n");
+                             //face.setExpression(RobotFaceWidget::Expression::Custom);
+                             //face.setCustomImage("/root/1.jpg");
                              break;
                          case HardwareHal::HeadPetGesture::Release:
-                             std::printf("gesture: release\n");
+                             //std::printf("gesture: release\n");
                              break;
                          case HardwareHal::HeadPetGesture::SwipeForward:
-                             std::printf("gesture: swipe forward\n");
+                             //std::printf("gesture: swipe forward\n");
+                             QMetaObject::invokeMethod(&startupWorker, restartStartupSequence,
+                                                       Qt::QueuedConnection);
                              break;
                          case HardwareHal::HeadPetGesture::SwipeBackward:
-                             std::printf("gesture: swipe backward\n");
+                             //std::printf("gesture: swipe backward\n");
                              break;
                          case HardwareHal::HeadPetGesture::None:
                              break;
